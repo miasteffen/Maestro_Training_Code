2e1986c610211051f4db13ec0ce1517017a9b1de
ViljamiB <Viljami.baker@studentssd.org>
2024-12-10 14:56:28 -0800

    Added StatusSignals to the EndEffector program.

--- src/main/java/frc/robot/subsystems/endEffector/EndEffector.java
+++ src/main/java/frc/robot/subsystems/endEffector/EndEffector.java
@@ -4,6 +4,7 @@
 
 package frc.robot.subsystems.endEffector;
 
+import com.ctre.phoenix6.StatusSignal;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.VoltageOut;
 import com.ctre.phoenix6.hardware.TalonFX;
@@ -19,6 +20,12 @@ public class EndEffector extends SubsystemBase {
   private final TalonFX motor = new TalonFX(Constants.CanIDs.END_EFFECTOR_CAN_ID);
 
   private final VoltageOut openLoopControl = new VoltageOut(0).withEnableFOC(true);
+
+  private final StatusSignal<Double> deviceTemp;
+  private final StatusSignal<Double> appliedVolts;
+  private final StatusSignal<Double> currentAmps;
+  private final StatusSignal<Double> velocityRPS;
+
   /** Creates a new EndEffector. */
   public EndEffector() {
     TalonFXConfiguration config = new TalonFXConfiguration();
@@ -31,6 +38,11 @@ public class EndEffector extends SubsystemBase {
     config.Feedback.SensorToMechanismRatio = Constants.MotorConstants.EndEffectorConstants.GEAR_RATIO;
 
     motor.getConfigurator().apply(config);
+
+    deviceTemp = motor.getDeviceTemp();
+    appliedVolts = motor.getMotorVoltage();
+    currentAmps = motor.getStatorCurrent();
+    velocityRPS = motor.getVelocity();
   }
 
   @Override
